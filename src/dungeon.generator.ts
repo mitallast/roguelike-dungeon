639a2f8c7213520465bea1456c1f38e5eac5fb7b
Aleksey Korchevsky <mitallast@gmail.com>
2020-03-03 22:44:09 +0300

    refactor dungeon generator - extract base class

--- src/dungeon.generator.ts
+++ src/dungeon.generator.ts
@@ -1,5 +1,101 @@
-import {DungeonLevel} from "./dungeon.level";
+import {DungeonLevel, DungeonZIndexes} from "./dungeon.level";
+import {RNG} from "./rng";
+import {DungeonScene} from "./dungeon";
+import {HeroState} from "./hero";
+import {TileRegistry} from "./tilemap";
+import {SceneController} from "./scene";
 
 export interface DungeonGenerator {
   generate(level: number): DungeonLevel;
+}
+
+export abstract class BaseDungeonGenerator implements DungeonGenerator {
+  protected readonly rng: RNG;
+  protected readonly registry: TileRegistry;
+  protected readonly controller: SceneController;
+  protected readonly scene: DungeonScene;
+  protected readonly heroState: HeroState;
+
+  protected constructor(scene: DungeonScene, heroState: HeroState) {
+    this.rng = scene.controller.rng;
+    this.registry = scene.controller.registry;
+    this.controller = scene.controller;
+    this.scene = scene;
+    this.heroState = heroState;
+  }
+
+  abstract generate(level: number): DungeonLevel;
+
+  protected replaceFloorRandomly(dungeon: DungeonLevel) {
+    const replacements = ['floor_2.png', 'floor_3.png', 'floor_4.png', 'floor_5.png', 'floor_6.png', 'floor_7.png', 'floor_8.png'];
+    const percent = 0.2;
+    for (let y = 0; y < dungeon.height; y++) {
+      for (let x = 0; x < dungeon.width; x++) {
+        if (dungeon.floorMap[y][x] && this.rng.nextFloat() < percent) {
+          dungeon.setFloor(x, y, this.rng.choice(replacements));
+        }
+      }
+    }
+  };
+
+  protected replaceWallRandomly(dungeon: DungeonLevel) {
+    const wall_mid_top_replaces = [
+      'wall_hole_1.png',
+      'wall_hole_2.png',
+      'wall_banner_red.png',
+      'wall_banner_blue.png',
+      'wall_banner_green.png',
+      'wall_banner_yellow.png',
+      'wall_goo.png',
+      'wall_fountain_mid_red',
+      'wall_fountain_mid_blue',
+    ];
+    const wall_mid_bottom_replaces = [
+      'wall_hole_1.png',
+      'wall_hole_2.png',
+    ];
+    const percent = 0.2;
+    for (let y = 0; y < dungeon.height; y++) {
+      for (let x = 0; x < dungeon.width; x++) {
+        if (dungeon.wallMap[y][x]) {
+          switch (dungeon.wallMap[y][x].name) {
+            case 'wall_mid.png':
+              if (this.rng.nextFloat() < percent) {
+                const is_top = !!dungeon.floorMap[y + 1][x];
+                let replacements: string[];
+                if (is_top) {
+                  replacements = wall_mid_top_replaces;
+                } else {
+                  replacements = wall_mid_bottom_replaces;
+                }
+                const replacement = this.rng.choice(replacements);
+                switch (replacement) {
+                  case 'wall_goo.png':
+                    dungeon.setWall(x, y, 'wall_goo.png', DungeonZIndexes.wallBack);
+                    dungeon.setFloor(x, y + 1, 'wall_goo_base.png');
+                    break;
+                  case 'wall_fountain_mid_red':
+                    dungeon.setWall(x, y - 1, 'wall_fountain_top.png', DungeonZIndexes.wallBack);
+                    dungeon.setWall(x, y, 'wall_fountain_mid_red', DungeonZIndexes.wallBack);
+                    dungeon.setFloor(x, y + 1, 'wall_fountain_basin_red');
+                    break;
+                  case 'wall_fountain_mid_blue':
+                    dungeon.setWall(x, y - 1, 'wall_fountain_top.png', DungeonZIndexes.wallBack);
+                    dungeon.setWall(x, y, 'wall_fountain_mid_blue', DungeonZIndexes.wallBack);
+                    dungeon.setFloor(x, y + 1, 'wall_fountain_basin_blue');
+                    break;
+                  default:
+                    dungeon.setWall(x, y, replacement, is_top ? DungeonZIndexes.wallBack : DungeonZIndexes.wallFront);
+                    break;
+                }
+              }
+              break;
+            default:
+              // console.log('replace', dungeon.wallMap[y][x]);
+              break;
+          }
+        }
+      }
+    }
+  }
 }
--- src/tunneling.generator.ts
+++ src/tunneling.generator.ts
@@ -1,30 +1,18 @@
 import {TinyMonster, tinyMonsterNames} from "./tiny.monster";
-import {RNG} from "./rng";
-import {TileRegistry} from "./tilemap";
 import {HeroState} from "./hero";
 import {BossMonster, mossMonsterNames} from "./boss.monster";
 import {DungeonScene} from "./dungeon";
-import {SceneController} from "./scene";
 import {Rect} from "./geometry";
 import {DungeonLevel, DungeonZIndexes} from "./dungeon.level";
 import {TunnelingAlgorithm} from "./tunneling";
-import {DungeonGenerator} from "./dungeon.generator";
+import {BaseDungeonGenerator} from "./dungeon.generator";
 
-const level_size = 200;
+export class TunnelingDungeonGenerator extends BaseDungeonGenerator {
+  private readonly level_size: number;
 
-export class TunnelingDungeonGenerator implements DungeonGenerator {
-  private readonly rng: RNG;
-  private readonly registry: TileRegistry;
-  private readonly controller: SceneController;
-  private readonly scene: DungeonScene;
-  private readonly heroState: HeroState;
-
-  constructor(scene: DungeonScene, heroState: HeroState) {
-    this.rng = scene.controller.rng;
-    this.registry = scene.controller.registry;
-    this.controller = scene.controller;
-    this.scene = scene;
-    this.heroState = heroState;
+  constructor(scene: DungeonScene, heroState: HeroState, level_size: number = 200) {
+    super(scene, heroState);
+    this.level_size = level_size;
   }
 
   generate(level: number): DungeonLevel {
@@ -32,7 +20,7 @@ export class TunnelingDungeonGenerator implements DungeonGenerator {
     const drop_total = 5 + level;
     const is_boss = level % 5 === 0;
 
-    const dungeon = new DungeonLevel(this.scene, this.heroState, level, level_size, level_size);
+    const dungeon = new DungeonLevel(this.scene, this.heroState, level, this.level_size, this.level_size);
 
     const rooms_total = 1 + dungeon.level;
     const gen = new TunnelingAlgorithm(this.rng, dungeon.width, dungeon.height);
@@ -439,84 +427,11 @@ export class TunnelingDungeonGenerator implements DungeonGenerator {
     }
   }
 
-  private replaceFloorRandomly(dungeon: DungeonLevel) {
-    const replacements = ['floor_2.png', 'floor_3.png', 'floor_4.png', 'floor_5.png', 'floor_6.png', 'floor_7.png', 'floor_8.png'];
-    const percent = 0.2;
-    for (let y = 0; y < dungeon.height; y++) {
-      for (let x = 0; x < dungeon.width; x++) {
-        if (dungeon.floorMap[y][x] && this.rng.nextFloat() < percent) {
-          dungeon.setFloor(x, y, this.rng.choice(replacements));
-        }
-      }
-    }
-  };
-
   private replaceLadder(dungeon: DungeonLevel, gen: TunnelingAlgorithm) {
     // replace one tile in last room as ladder = out from level!
     const last = gen.rooms[gen.rooms.length - 1];
     const ladder_x = last.x + (last.w >> 1);
     const ladder_y = last.y + (last.h >> 1);
     dungeon.setFloor(ladder_x, ladder_y, 'floor_ladder.png');
-  };
-
-  private replaceWallRandomly(dungeon: DungeonLevel) {
-    const wall_mid_top_replaces = [
-      'wall_hole_1.png',
-      'wall_hole_2.png',
-      'wall_banner_red.png',
-      'wall_banner_blue.png',
-      'wall_banner_green.png',
-      'wall_banner_yellow.png',
-      'wall_goo.png',
-      'wall_fountain_mid_red',
-      'wall_fountain_mid_blue',
-    ];
-    const wall_mid_bottom_replaces = [
-      'wall_hole_1.png',
-      'wall_hole_2.png',
-    ];
-    const percent = 0.2;
-    for (let y = 0; y < dungeon.height; y++) {
-      for (let x = 0; x < dungeon.width; x++) {
-        if (dungeon.wallMap[y][x]) {
-          switch (dungeon.wallMap[y][x].name) {
-            case 'wall_mid.png':
-              if (this.rng.nextFloat() < percent) {
-                const is_top = !!dungeon.floorMap[y + 1][x];
-                let replacements: string[];
-                if (is_top) {
-                  replacements = wall_mid_top_replaces;
-                } else {
-                  replacements = wall_mid_bottom_replaces;
-                }
-                const replacement = this.rng.choice(replacements);
-                switch (replacement) {
-                  case 'wall_goo.png':
-                    dungeon.setWall(x, y, 'wall_goo.png', DungeonZIndexes.wallBack);
-                    dungeon.setFloor(x, y + 1, 'wall_goo_base.png');
-                    break;
-                  case 'wall_fountain_mid_red':
-                    dungeon.setWall(x, y - 1, 'wall_fountain_top.png', DungeonZIndexes.wallBack);
-                    dungeon.setWall(x, y, 'wall_fountain_mid_red', DungeonZIndexes.wallBack);
-                    dungeon.setFloor(x, y + 1, 'wall_fountain_basin_red');
-                    break;
-                  case 'wall_fountain_mid_blue':
-                    dungeon.setWall(x, y - 1, 'wall_fountain_top.png', DungeonZIndexes.wallBack);
-                    dungeon.setWall(x, y, 'wall_fountain_mid_blue', DungeonZIndexes.wallBack);
-                    dungeon.setFloor(x, y + 1, 'wall_fountain_basin_blue');
-                    break;
-                  default:
-                    dungeon.setWall(x, y, replacement, is_top ? DungeonZIndexes.wallBack : DungeonZIndexes.wallFront);
-                    break;
-                }
-              }
-              break;
-            default:
-              // console.log('replace', dungeon.wallMap[y][x]);
-              break;
-          }
-        }
-      }
-    }
   };
 }
