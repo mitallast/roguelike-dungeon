ff0a5675fbd72b9da34b6cf353104403d456d05f
mitallast <mitallast@gmail.com>
2020-05-15 11:17:13 +0300

    fix terser mangle options

--- pixi.declares.sh
+++ pixi.declares.sh
@@ -2,21 +2,31 @@
 
 function process() {
   file="$1"
-  echo scan $file
   cat "$file" | sed -E 's#\/\/.+##g' | sed 's|/\*|\n&|g;s|*/|&\n|g' | sed '/\/\*/,/*\//d' |\
+    sed -E 's/([a-zA-Z_]+)\??\([^\)]+\)\:/\1/g' |\
     sed -E 's#"[^"]+"##g' | sed -E "s#'[^']+'##g" |\
-    sed -E 's/(declare|namespace|var|class|extends|constructor|number|readonly|object|type|Array|symbol)//g' |\
-    sed -E 's/(export|enum|let|string|function|void|any|boolean|this|module|interface|index|undefined|canvas)//g' |\
-    sed -E 's/(Float32Array|Uint32Array|Uint16Array|static|x|y|CanvasGradient|CanvasPattern)//g' |\
-    sed -E 's/(HTML|XML)[A-Za-z]+|)//g'
-  # grep -oE '[a-zA-Z0-9_]+'
+    grep -oE '[a-zA-Z0-9_]+' |\
+    grep -vE '^(declare|namespace|var|class|extends|constructor|number|readonly|object|type|Array|symbol)$' |\
+    grep -vE '^(export|enum|let|string|function|void|any|boolean|this|module|interface|index|undefined|canvas)$' |\
+    grep -vE '^(Float32Array|Uint32Array|Uint16Array|static|x|y|CanvasGradient|CanvasPattern)$' |\
+    grep -vE '^(HTML|XML)[A-Za-z]+|)$' |\
+    grep -vE '^.$' |\
+    sort -u
 }
 
-#process "./node_modules/pixi-layers/dist/pixi-layers.d.ts"
-#process "./node_modules/pixi-sound/pixi-sound.d.ts"
-process "./node_modules/pixi.js/pixi.js.d.ts"
+echo '[' > reserved.json
 
-#find ./node_modules/pixi* -name '*.d.ts' -print0 | while read -d $'\0' file
-#do
-#  process "$file"
-#done
+find ./node_modules/pixi* -name '*.d.ts' -print0 | while read -d $'\0' file
+do
+  process "$file"
+done | sort -u | xargs -I{} echo '"{}",' >> reserved.json
+
+
+cat dungeon.design.json | grep -oE '"[a-zA-Z0-9_]+":' | tr ':' ',' >> reserved.json
+cat dungeon.rules.json | grep -oE '"[a-zA-Z0-9_]+":' | tr ':' ',' >> reserved.json
+cat monster.config.json | grep -oE '"[a-zA-Z0-9_]+":' | tr ':' ',' >> reserved.json
+cat npc.config.json | grep -oE '"[a-zA-Z0-9_]+":' | tr ':' ',' >> reserved.json
+cat weapon.config.json | grep -oE '"[a-zA-Z0-9_]+":' | tr ':' ',' >> reserved.json
+cat dialogs.json | grep -oE '"[a-zA-Z0-9_]+":' | tr ':' ',' >> reserved.json
+
+echo '"PIXI"]' >> reserved.json
